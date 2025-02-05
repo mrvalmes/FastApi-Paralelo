91cc59a1e8d0f9b9d23090bd069c756db646681b
mrvalmes <124213300+mrvalmes@users.noreply.github.com>
2025-02-04 21:55:19 -0400

    4 feb 2025
    Actualizacion, Servicio

--- app/notifications.py
+++ app/notifications.py
@@ -1,9 +1,9 @@
 # app/notifications.py
 import requests
 
-# Asegúrate de usar los valores correctos para tu aplicación OneSignal.
+# Api Key y App ID de OneSignal
 ONESIGNAL_APP_ID = "dc17cf54-a-b30a-bf30f60cc265" 
-ONESIGNAL_API_KEY = "os_v2_app_3ql46vfo4jaizmykx4ypmf3xw5fvnna7aofq5abtttljjffhutfmuwjrk66ya"  # Se corrigió la comilla y se definió correctamente la cadena
+ONESIGNAL_API_KEY = "os_v2_app_3ql46vfo4jaizmykx4ypmf3xw5fvnna7aofq5abtttljjffhutfmuwjrk66ya"  
 
 def send_push_notification(task_id: int):    
     url = "https://onesignal.com/api/v1/notifications"
@@ -14,7 +14,7 @@ def send_push_notification(task_id: int):
     }
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Basic {ONESIGNAL_API_KEY}"  # Se agregó un espacio luego de "Basic" si es necesario: "Basic {ONESIGNAL_API_KEY}"
+        "Authorization": f"Basic {ONESIGNAL_API_KEY}"
     }
     response = requests.post(url, json=payload, headers=headers)
     # Imprime información para depuración
--- app/task.py
+++ app/task.py
@@ -3,7 +3,7 @@ import time
 from app.notifications import send_push_notification
 
 def process_task(task_id: int):
-    # Simular una tarea intensiva (procesamiento o cálculos)
+    #tarea intensiva
     time.sleep(5)
     print(f"Tarea {task_id} completada")
     send_push_notification(task_id)
